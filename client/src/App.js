d05b2641a970e2b25a6f23e3b08935ddddbb5cd7
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-03-18 22:34:19 +0200

    mock data for displaying pokemon

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
 import "./styles/App.css";
+import Pokemon from "./components/Pokemon";
+import React, { useState } from "react";
 
 function App() {
+	const [pokemonData, setPokemonData] = useState({
+		id: 12,
+		name: "butterfree",
+		base_experience: 178,
+		height: 11,
+		is_default: true,
+		order: 16,
+		weight: 320,
+	});
+
 	return (
 		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			<h1>Pokedex</h1>
+			<input id="search-input" />
+			<button id="search-button">Search Pokemon!</button>
+			<Pokemon pokemonData={pokemonData} setPokemonData={setPokemonData} />
 		</div>
 	);
 }
