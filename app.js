6c1ad92489cacc8ff466943f70b6059efe76f6cc
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-03-19 16:48:42 +0200

    add morgan logging to backend

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const api = require("./routes");
 const app = express();
+const morgan = require("morgan");
 
 app.use(cors());
 app.use(express.json());
+
+morgan.token("person", (req) => {
+	return JSON.stringify(req.body);
+});
+
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :person"));
 
 app.use(express.static("build"));
 
