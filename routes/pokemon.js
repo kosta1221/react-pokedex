5c2d510fe465e5bac611a7aad3744881f7517c83
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-03-19 16:48:19 +0200

    add pokemon route

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,9 +1,25 @@
 const { Router } = require("express");
 
-const pokemon = Router();
+const axios = require("axios");
 
-pokemon.get("/", (req, res) => {
-  res.send("Pokemon route");
+const pokemon = Router();
+const POKEAPI_URL = `https://pokeapi.co/api/v2/pokemon`;
+
+pokemon.get(`/:name`, async (req, res) => {
+	const { data } = await axios({
+		method: "GET",
+		url: `${POKEAPI_URL}/${req.params.name}`,
+		headers: { "Content-Type": "application/json" },
+	});
+	const relevantData = {
+		id: data.id,
+		name: data.name,
+		height: data.height,
+		weight: data.weight,
+		types: data.types.map((type) => type.type.name),
+	};
+	console.log(`Pokemon route + ${req.params.name}`);
+	res.json(relevantData);
 });
 
 module.exports = pokemon;
